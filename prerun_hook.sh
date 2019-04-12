0994e64121300372ab4a852b8ab57e1848826719
Jacob Pargin <hello@jacobpgn.com>
2019-04-12 19:42:16 +0100

    Make mysqladmin be quiet about errors

--- prerun_hook.sh
+++ prerun_hook.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-while ! mysqladmin ping -h"${MYSQL_WAIT_FOR_HOST}" -P"${MYSQL_WAIT_FOR_PORT}"; do
+while ! mysqladmin ping -h"${MYSQL_WAIT_FOR_HOST}" -P"${MYSQL_WAIT_FOR_PORT}" --silent; do
   sleep 1
 done

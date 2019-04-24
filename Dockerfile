9cbf56b53ad5a87f226d99f4a44cb850c48f8535
Jacob Pargin <hello@jacobpgn.com>
2019-04-24 12:48:53 +0100

    Use nc instead of mysqladmin

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 ARG tag
 FROM prismagraphql/prisma:$tag
 
-RUN apk update
-RUN apk add mariadb-client
-
 COPY ./prerun_hook.sh /app/prerun_hook.sh
 
 CMD /app/start.sh
--- prerun_hook.sh
+++ prerun_hook.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-while ! mysqladmin ping -h"${MYSQL_WAIT_FOR_HOST}" -P"${MYSQL_WAIT_FOR_PORT}" --silent; do
+while ! nc -z $WAIT_FOR_HOST $WAIT_FOR_PORT; do
   sleep 1
 done
